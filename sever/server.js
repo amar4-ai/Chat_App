40109a39244d75ae9dd03ad13f0e1e2e838cb004
Amar <amarkhadkabardiya1234@gmail.com>
2026-03-28 14:57:12 +0545

    add vercel.json config for vercel deployment

--- sever/server.js
+++ sever/server.js
@@ -17,21 +17,21 @@ const server = http.createServer(app)
 
 //Initialize socket.io server
 export const io = new Server(server, {
-    cors : {origin: "*"}
+    cors: { origin: "*" }
 })
 
 //store online users
 export const userSocketMap = {}; //{userId : socketId}
 
 //Scoket.io connection handler
-io.on("connection",(socket)=>{
+io.on("connection", (socket) => {
     const userId = socket.handshake.query.userId;
     console.log("User Connected", userId);
-    if(userId) userSocketMap[userId]= socket.id
+    if (userId) userSocketMap[userId] = socket.id
 
     //Emit online usres to all connected client
     io.emit("getOnlineUsers", Object.keys(userSocketMap));
-    socket.on("disconnect", ()=>{
+    socket.on("disconnect", () => {
         console.log("User Disconnected", userId);
         delete userSocketMap[userId];
         io.emit("getOnlineUsers", Object.keys(userSocketMap));
@@ -40,11 +40,11 @@ io.on("connection",(socket)=>{
 
 
 //Middlerware Setup
-app.use(express.json({limit:"4mb"}));
+app.use(express.json({ limit: "4mb" }));
 app.use(cors());
 
 //Routes Setup
-app.use("/api/status",(req,res)=> res.send("Server is live"));
+app.use("/api/status", (req, res) => res.send("Server is live"));
 app.use("/api/auth", userRouter);
 app.use("/api/messages", messageRouter);
 
@@ -52,6 +52,10 @@ app.use("/api/messages", messageRouter);
 //Connect to MongoDb
 await connetDB();
 
+if (process.env.NODE_ENV !== "production") {
+    const PORT = process.env.PORT || 5000;
+    server.listen(PORT, () => console.log("Server is running on PORT:" + PORT));
 
-const PORT = process.env.PORT || 5000;
-server.listen(PORT, ()=> console.log("Server is running on PORT:" + PORT));
+}
+// Export server for Vercel
+export default server;
